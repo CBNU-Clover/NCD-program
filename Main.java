7062d09fcd038b7ae99b55e71f49fff4701efd94
LEE026 <lhs456852@gmail.com>
2022-11-28 21:53:41 +0900

    FEET: json을 브릿지 코드의 블럭 간의 연결을 나타내는 부분으로 변환

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import code_maker.CodeMaker;
 import code_maker.Cpp_CodeMaker;
+import code_maker.DiagramModelToBridgeCode;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
@@ -7,6 +8,9 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        DiagramModelToBridgeCode bridgeCode=new DiagramModelToBridgeCode();
+        System.out.println(bridgeCode.getBridgeCode());
+        /*
         try {
             Cpp_CodeMaker codeMaker=new Cpp_CodeMaker("./src/test/testFile4.json");
             System.out.println(codeMaker.makeCode());
@@ -15,5 +19,6 @@ public class Main {
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+         */
     }
 }
--- code_maker/DiagramModelToBridgeCode.java
+++ code_maker/DiagramModelToBridgeCode.java
@@ -1,5 +1,71 @@
 package code_maker;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class DiagramModelToBridgeCode {
 
+    public JSONObject getJson(String fileName){
+        JSONObject obj=null;
+        try {
+            Reader reader = new FileReader(fileName);
+            JSONParser parser = new JSONParser();
+            obj = (JSONObject) parser.parse(reader);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return obj;
+    }
+
+    public JSONObject getBridgeCode(){
+
+        JSONObject diagram= getJson("src/json_test/testFile.json");
+
+
+
+        Map<Long,JSONObject> blocks=new HashMap<Long,JSONObject>();
+        JSONArray blocklist=(JSONArray) diagram.get("nodeDataArray");
+        for(int i=0;i<blocklist.size();i++){
+            JSONObject code=new JSONObject();
+            JSONObject block=(JSONObject) blocklist.get(i);
+            Long id=(Long) block.get("key");
+            code.put("BlockType",(String) block.get("category"));
+            code.put("BlockID",id);
+
+            blocks.put(id,code);
+        }
+
+        JSONArray links = (JSONArray) diagram.get("linkDataArray");
+
+        for(int i=0;i<links.size();i++){
+            JSONObject edge=(JSONObject)links.get(i);
+            Long from=(Long) edge.get("from");
+            Long to=(Long) edge.get("to");
+
+            JSONObject block=blocks.get(from);
+            block.put("NextBlockID",to);
+            blocks.put(from,block);
+        }
+
+        JSONArray jsonArray=new JSONArray();
+
+        Iterator<Map.Entry<Long,JSONObject>> entryIterator=blocks.entrySet().iterator();
+        while (entryIterator.hasNext()){
+            Map.Entry<Long,JSONObject> element =entryIterator.next();
+            jsonArray.add(element.getValue());
+        }
+
+        JSONObject result=new JSONObject();
+        result.put("blocklist",jsonArray);
+        return result;
+    }
 }
